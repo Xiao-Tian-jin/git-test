4702338ec5962e5319930be941a7160fafdbae0e
LiuJin <1501036030@qq.com>
2022-02-17 14:34:27 +0800

    first commit

---
+++ src/main/java/com/cn/git/GitTest1.java
@@ -0,0 +1,11 @@
+package com.cn.git;
+
+/**
+ * @author xiao`tian
+ * @create 2022-02-17 14:31
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
