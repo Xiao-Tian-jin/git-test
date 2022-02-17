5f9c59a6299fb107cffae6e311e1cf2d8871fbc8
LiuJin <1501036030@qq.com>
2022-02-17 18:53:30 +0800

    hot-fix commit2

--- src/main/java/com/cn/git/GitTest1.java
+++ src/main/java/com/cn/git/GitTest1.java
@@ -10,5 +10,6 @@ public class GitTest1 {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hto-fix test");
     }
 }
