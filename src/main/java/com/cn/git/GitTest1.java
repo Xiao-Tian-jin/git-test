34b034bbd19b2619af2d63547d57866db1e39030
LiuJin <1501036030@qq.com>
2022-02-17 18:10:56 +0800

    second commit

--- src/main/java/com/cn/git/GitTest1.java
+++ src/main/java/com/cn/git/GitTest1.java
@@ -7,5 +7,6 @@ package com.cn.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
